7cbcad4b2853a8fa5d6c79251ae065a78c52008e
viscolex <37867692+viscolex@users.noreply.github.com>
2018-03-28 23:13:07 +0100

    Update solaris-update.sh

--- crypto/solaris-update/solaris-update.sh
+++ crypto/solaris-update/solaris-update.sh
@@ -2,6 +2,8 @@
 echo "Solaris VPS update"
 cd solaris
 ./solarisd -stop
-wget https://github.com/Solaris-Project/Solaris/releases/download/v2.5.0.0/solaris-daemon-2.5.0.0-linux64.tar.gz
-tar xzvf solaris-daemon-2.5.0.0-linux64.tar.gz
+./solarisd -stop
+./solarisd -stop
+wget https://github.com/Solaris-Project/Solaris/releases/download/v2.6.0.0/solaris-daemon-2.6.0.0-linux64.tar.gz
+tar xzvf solaris-daemon-2.6.0.0-linux64.tar.gz
 ./solarisd -daemon
