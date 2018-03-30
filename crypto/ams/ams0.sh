276d3ff65e6a62b8bdc6b4d5329fbde798e84a95
viscolex <viscolex@hotmail.com>
2018-03-30 01:31:59 +0100

    code fix

--- crypto/ams/ams0.sh
+++ crypto/ams/ams0.sh
@@ -4,7 +4,7 @@ apt update
 apt upgrade
 sudo apt-get install nano
 mkdir ams
-cd ams 
+cd ams
 wget https://github.com/CoinProjects/AmsterdamCoin-v4/releases/download/v4.1.0.0/amsterdamcoin-daemon-4.1.0.0-linux64.tar.gz
 tar xzvf amsterdamcoin-daemon-4.1.0.0-linux64.tar.gz
 ./amsterdamcoind -daemon
