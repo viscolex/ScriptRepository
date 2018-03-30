77b560fdfe857464f4399dacaaaf24aae73f8e6a
viscolex <visolex@hotmail.com>
2018-03-30 17:27:17 +0100

    redirect

--- crypto/Solaris/solaris0.sh
+++ crypto/Solaris/solaris0.sh
@@ -8,6 +8,6 @@ cd solaris
 wget https://github.com/Solaris-Project/Solaris/releases/download/v2.6.0.0/solaris-daemon-2.6.0.0-linux64.tar.gz
 tar xzvf solaris-daemon-2.6.0.0-linux64.tar.gz
 ./solarisd -daemon
-wget https://raw.githubusercontent.com/HysMagus/ScriptRepository/master/crypto/masternode/solaris1.sh
+wget https://raw.githubusercontent.com/viscolex/ScriptRepository/master/crypto/masternode/solaris1.sh
 chmod +x solaris1.sh
 sudo bash solaris1.sh
--- crypto/Solaris/solaris1.sh
+++ crypto/Solaris/solaris1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-wget -O /root/.solaris/masternode.conf https://raw.githubusercontent.com/HysMagus/ScriptRepository/master/crypto/masternode/masternode.conf
+wget -O /root/.solaris/masternode.conf https://raw.githubusercontent.com/viscolex/ScriptRepository/master/crypto/masternode/masternode.conf
 echo "Copy the contents from your local Masternode.conf to the VPS"
 nano /root/.solaris/masternode.conf
 echo "Copy the contents from your local solaris.conf to the VPS"
-wget -O /root/.solaris/solaris.conf https://raw.githubusercontent.com/HysMagus/ScriptRepository/master/crypto/masternode/solaris.conf
+wget -O /root/.solaris/solaris.conf https://raw.githubusercontent.com/viscolex/ScriptRepository/master/crypto/masternode/solaris.conf
 nano /root/.solaris/solaris.conf
 ./solarisd -daemon
