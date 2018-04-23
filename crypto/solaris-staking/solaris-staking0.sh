ec5c1c6a26ad5342e6561c5c61e4d9bc4256b1f2
viscolex <37867692+viscolex@users.noreply.github.com>
2018-04-23 12:54:39 +0100

    Update solaris-staking0.sh

--- crypto/solaris-staking/solaris-staking0.sh
+++ crypto/solaris-staking/solaris-staking0.sh
@@ -5,8 +5,8 @@ apt upgrade
 sudo apt-get install nano
 mkdir solaris
 cd solaris 
-wget https://github.com/Solaris-Project/Solaris/releases/download/v2.6.0.0/solaris-daemon-2.6.0.0-linux64.tar.gz
-tar xzvf solaris-daemon-2.6.0.0-linux64.tar.gz
+wget https://github.com/Solaris-Project/Solaris/releases/download/v2.8.0.0/solaris-daemon-2.8.0.0-linux64.tar.gz
+tar xzvf solaris-daemon-2.8.0.0-linux64.tar.gz
 ./solarisd -daemon
 wget https://raw.githubusercontent.com/viscolex/ScriptRepository/master/crypto/solaris-staking/solaris-staking1.sh
 chmod +x solaris-staking1.sh
