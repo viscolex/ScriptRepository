a8337584221fdd67f431637d70f7f1937c86cbdb
viscolex <37867692+viscolex@users.noreply.github.com>
2018-04-23 23:27:38 +0100

    Update solaris0.sh

--- crypto/Solaris/solaris0.sh
+++ crypto/Solaris/solaris0.sh
@@ -10,6 +10,6 @@ cd solaris
 wget https://github.com/Solaris-Project/Solaris/releases/download/v2.8.0.0/solaris-daemon-2.8.0.0-linux64.tar.gz
 tar xzvf solaris-daemon-2.8.0.0-linux64.tar.gz
 ./solarisd -daemon
-wget https://raw.githubusercontent.com/viscolex/ScriptRepository/master/crypto/masternode/solaris1.sh
+wget https://raw.githubusercontent.com/viscolex/ScriptRepository/master/crypto/Solaris/solaris1.sh
 chmod +x solaris1.sh
 sudo bash solaris1.sh
