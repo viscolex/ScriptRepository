7fb65eea888fda0511d6f655adbeecb30a381106
viscolex <37867692+viscolex@users.noreply.github.com>
2018-11-06 21:39:46 +0000

    Create solaris-mn-test.sh

---
+++ solaris/solaris-mn-test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo "Solaris Install Part 1"
+apt update
+apt upgrade
+
+echo "Install nano and curl"
+sudo apt-get install -y nano
+sudo apt-get install -y curl
+echo "Part 2, download wallet"
+mkdir /root/solaris
+cd /root/solaris
+wget https://github.com/Solaris-Project/Solaris/releases/download/v2.8.1.0/solaris-daemon-2.8.1.0-linux64.tar.gz
+tar xzvf solaris-daemon-2.8.1.0-linux64.tar.gz
+cp solarisd solaris-cli /usr/local/bin
+
+solarisd -daemon
+
+echo "Part 3, create solaris.conf and start daemon"
+IP=$(curl http://checkip.amazonaws.com/)
+PW=$(date +%s | sha256sum | base64 | head -c 32 ;)
+echo "==========================================================="
+pwd
+echo "rpcallowip=127.0.0.1" >> /root/.solaris/solaris.conf
+echo "rpcuser=solarisusernodesupply">> /root/.solaris/solaris.conf
+echo "rpcpassword="$PW >> /root/.solaris/solaris.conf
+echo "daemon=1" >> /root/.solaris/solaris.conf
+echo "testnet=1" >> /root/.solaris/solaris.conf
+echo "staking=1" >> /root/.solaris/solaris.conf
+echo "listen=1" >> /root/.solaris/solaris.conf
+echo "server=1" >>/root/.solaris/solaris.conf
+echo "externalip="$IP":60020" >>/root/.solaris/solaris.conf
+echo "masternodeaddr="$IP":60020" >>/root/.solaris/solaris.conf
+echo "masternode=1" >>/root/.solaris/solaris.conf
+echo "masternodeprivkey="$1 >>/root/.solaris/solaris.conf
+cat /root/.solaris/solaris.conf
+
+solarisd
