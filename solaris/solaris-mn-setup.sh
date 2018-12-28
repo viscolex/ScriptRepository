d7ebc7e86b391822995d232b281ce8331f8ddb49
viscolex <37867692+viscolex@users.noreply.github.com>
2018-12-28 00:31:37 +0000

    Update solaris-mn-setup.sh

--- solaris/solaris-mn-setup.sh
+++ solaris/solaris-mn-setup.sh
@@ -3,10 +3,23 @@ echo "Solaris Install Part 1"
 apt update
 apt upgrade
 
+
+echo "Solaris Install Part 2"
+echo "Creating Swap File..."
+echo "This may take a few minutes, please be patient..."
+
+sudo dd if=/dev/zero of=/swapfile bs=1024 count=2097152
+chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+
 echo "Install nano and curl"
 sudo apt-get install -y nano
 sudo apt-get install -y curl
-echo "Part 2, download wallet"
+echo "Solaris Install Part 3"
+echo "Download wallet"
 mkdir /root/solaris
 cd /root/solaris
 wget https://github.com/Solaris-Project/Solaris/releases/download/v2.8.1.0/solaris-daemon-2.8.1.0-linux64.tar.gz
@@ -15,7 +28,8 @@ cp solarisd solaris-cli /usr/local/bin
 
 solarisd
 
-echo "Part 3, create solaris.conf and start daemon"
+echo "Solaris Install Part 3"
+echo "Creating solaris.conf and starting daemon..."
 IP=$(curl http://checkip.amazonaws.com/)
 PW=$(date +%s | sha256sum | base64 | head -c 32 ;)
 echo "==========================================================="
@@ -34,3 +48,4 @@ echo "masternodeprivkey="$1 >>/root/.solaris/solaris.conf
 cat /root/.solaris/solaris.conf
 
 solarisd -daemon
+echo "Installation complete"
