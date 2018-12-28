4fecd32238d2cde476bd96929ce9ab55cccb88b2
viscolex <37867692+viscolex@users.noreply.github.com>
2018-12-28 00:18:29 +0000

    Update addswap.sh

--- solaris/addswap.sh
+++ solaris/addswap.sh
@@ -5,6 +5,8 @@ echo "This may take a few minutes, please be patient."
 
 sudo dd if=/dev/zero of=/swapfile bs=1024 count=2097152
 
+chmod 600 /swapfile
+
 sudo mkswap /swapfile
 
 sudo swapon /swapfile
